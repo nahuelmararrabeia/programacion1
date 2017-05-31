e600e8e6be47d5b48a378a697e81158de6160236
nahuelmararrabiea <nahuelmm5@gmail.com>
2017-05-31 17:06:59 -0300

    Add files via upload

--- TP3/funciones.c
+++ TP3/funciones.c
@@ -89,9 +89,10 @@ void generarPagina()
     FILE* aRchHTML;
     int tam,i;
     tam=cantidadRegistros();
-    EMovie *movies;
+    //EMovie *movies;
     EMovie movie[tam];
-    movies=movie;
+    //movies=movie;
+    //movies=((EMovie*)malloc(tam*sizeof(EMovie)));
 
     if((aRch=fopen("movies.dat","rb+"))==NULL)
         {
@@ -110,15 +111,18 @@ void generarPagina()
                  return 0;
              }
         }
-
+    fprintf(aRchHTML,"%s", "<!DOCTYPE html>\n<html lang='en'>\n<head>\n    <meta charset='utf-8'>\n    <meta http-equiv='X-UA-Compatible' content='IE=edge'>\n    <meta name='viewport' content='width=device-width, initial-scale=1'>\n    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->\n    <title>Lista peliculas</title>\n    <!-- Bootstrap Core CSS -->\n    <link href='css/bootstrap.min.css' rel='stylesheet'>\n    <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->\n    <link href='css/custom.css' rel='stylesheet'>\n    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->\n    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->\n    <!--[if lt IE 9]>\n        <script src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script>\n        <script src='https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'></script>\n    <![endif]-->\n</head>\n<body>\n    <div class='container'>\n        <div class='row'>\n\n		" );
     for(i=0;i<tam;i++)
     {
-        fread(movie[i],EMovie, 1, aRch);
+        fread(&movie[i], sizeof(EMovie), 1, aRch);
         fflush(stdin);
-        fprintf(aRchHTML,"%s", "<!DOCTYPE html>\n<html lang='en'>\n<head>\n    <meta charset='utf-8'>\n    <meta http-equiv='X-UA-Compatible' content='IE=edge'>\n    <meta name='viewport' content='width=device-width, initial-scale=1'>\n    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->\n    <title>Lista peliculas</title>\n    <!-- Bootstrap Core CSS -->\n    <link href='css/bootstrap.min.css' rel='stylesheet'>\n    <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->\n    <link href='css/custom.css' rel='stylesheet'>\n    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->\n    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->\n    <!--[if lt IE 9]>\n        <script src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script>\n        <script src='https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'></script>\n    <![endif]-->\n</head>\n<body>\n    <div class='container'>\n        <div class='row'>\n\n		" );
-        fprintf(aRchHTML,"%s", "\n            <article class='col-md-4 article-intro'>\n                <a href='#'>\n                    <img class='img-responsive img-rounded' src='%s' alt=''>\n                </a>\n                <h3>\n                    <a href='#'>%s</a>\n                </h3>\n				<ul>\n					<li>Género:%s</li>\n					<li>Puntaje:%d</li>\n					<li>Duración:%d</li>					\n				</ul>\n               <p>%s</p>\n            </article>",*(movies+i)->linkImagen,*(movies+i)->titulo,*(movies+i)->genero,*(movies+i)->puntaje,*(movies+i)->duracion,*(movies+i)->descripcion);
-        fprintf(aRchHTML,"%s","\n        </div>\n        <!-- /.row -->\n    </div>\n    <!-- /.container -->\n    <!-- jQuery -->\n    <script src='js/jquery-1.11.3.min.js'></script>\n    <!-- Bootstrap Core JavaScript -->\n    <script src='js/bootstrap.min.js'></script>\n	<!-- IE10 viewport bug workaround -->\n	<script src='js/ie10-viewport-bug-workaround.js'></script>\n	<!-- Placeholder Images -->\n	<script src='js/holder.min.js'></script>\n</body>\n</html>");
+
+        fprintf(aRchHTML,"\n            <article class='col-md-4 article-intro'>\n                <a href='#'>\n                    <img class='img-responsive img-rounded' src=' %s ' alt=''>\n                </a>\n                <h3>\n                    <a href='#'> %s </a>\n                </h3>\n				<ul>\n					<li>Género: %s </li>\n					<li>Puntaje: %d </li>\n					<li>Duración: %d </li>					\n				</ul>\n               <p> %s </p>\n            </article>" ,movie[i].linkImagen,movie[i].titulo,movie[i].genero,movie[i].puntaje,movie[i].duracion,movie[i].descripcion);
+
     }
+    fprintf(aRchHTML,"%s","\n        </div>\n        <!-- /.row -->\n    </div>\n    <!-- /.container -->\n    <!-- jQuery -->\n    <script src='js/jquery-1.11.3.min.js'></script>\n    <!-- Bootstrap Core JavaScript -->\n    <script src='js/bootstrap.min.js'></script>\n	<!-- IE10 viewport bug workaround -->\n	<script src='js/ie10-viewport-bug-workaround.js'></script>\n	<!-- Placeholder Images -->\n	<script src='js/holder.min.js'></script>\n</body>\n</html>");
+    fclose(aRch);
+    fclose(aRchHTML);
 
 
 
@@ -127,43 +131,6 @@ void generarPagina()
 
 
 
-
-
-
-
-
-/*void generarPagina(EMovie* lista, char *nombre)
-{
-    int i,tam;
-    long int ;
-    tam=cantidadRegistros();
-    FILE *aRch;
-    EMovie* arrayMovies[tam];
-    if((aRch=fopen("movies.dat","rb+"))==NULL)
-        {
-             printf("\nEl archivo no puede ser abierto");
-             return 0;
-        }
-    rewind(aRch);
-     while(!feof(aRch))
-    {
-        if(!feof(aRch))
-            break;
-
-        fread(&movie, sizeof(EMovie),1,aRch);
-        tam++;
-    }
-    //fseek(aRch, 0L, SEEK_END);
-
-
-    for(i=0;i<tam;i++)
-    {
-        fflush(stdin);
-        printf("%s\t%s\t%s\t%d\t%d", lista->titulo, lista->genero,lista->descripcion,lista->duracion,lista->puntaje);
-    }
-    printf("%d", &tam);
-}
-*/
 int cantidadRegistros()
 {
     int tam=0;
